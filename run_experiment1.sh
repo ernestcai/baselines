cff6e2c76779b4c54b59dae10aba7b2890e5ad61
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2019-01-16 23:20:34 +0800

    save the running script

---
+++ run_experiment1.sh
@@ -0,0 +1,11 @@
+obj_range=0.17
+target_range=0.17
+OPENAI_LOGDIR=$HOME/logs/cube/fetchpush-v1-$obj_range-$target_range OPENAI_LOG_FORMAT=csv python -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=750000 --save_path=~/policies/her/fetchpush5k-$obj_range-$target_range --obj_range=$obj_range --target_range=$target_range
+
+obj_range=0.13
+target_range=0.08
+OPENAI_LOGDIR=$HOME/logs/cube/fetchpush-v1-$obj_range-$target_range OPENAI_LOG_FORMAT=csv python -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=750000 --save_path=~/policies/her/fetchpush5k-$obj_range-$target_range --obj_range=$obj_range --target_range=$target_range
+
+obj_range=0.17
+target_range=0.08
+OPENAI_LOGDIR=$HOME/logs/cube/fetchpush-v1-$obj_range-$target_range OPENAI_LOG_FORMAT=csv python -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=750000 --save_path=~/policies/her/fetchpush5k-$obj_range-$target_range --obj_range=$obj_range --target_range=$target_range
